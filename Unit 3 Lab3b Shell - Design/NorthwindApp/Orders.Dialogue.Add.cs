1c4e2932f7793ab96207ed5e8efb50d966c58e31
HassanBaji <hassan.udyh@gmail.com>
2023-03-15 00:39:42 +0300

    1

--- Unit 3 Lab3b Shell - Design/NorthwindApp/Orders.Dialogue.Add.cs
+++ Unit 3 Lab3b Shell - Design/NorthwindApp/Orders.Dialogue.Add.cs
@@ -53,47 +53,11 @@ namespace NorthwindApp
             ddlShipVia.ValueMember = "ShipperID";
             ddlShipVia.SelectedItem = null;
 
-            //if edit order, poplate the text boxes 
-            if (order != null)
+         
+            if (order.OrderId > 0)
             {
-                // Set the selected value of the customer dropdown list to the customer ID of the order
-                ddlCustomer.SelectedValue = order.CustomerId;
-
-                // Set the selected value of the employee dropdown list to the employee ID of the order
-                ddlEmployee.SelectedValue = order.EmployeeId;
-
-                // Set the value of the order date picker to the order date of the order
-                dtpOrderDate.Value = order.OrderDate.Value;
-
-                // Set the value of the required date picker to the required date of the order
-                dtpRequiredDate.Value = order.RequiredDate.Value;
-
-                // Set the value of the shipped date picker to the shipped date of the order if it's not null, or to the current date if it is null
-                dtpShippedDate.Value = order.ShippedDate ?? DateTime.Now;
-
-                // Set the checked state of the shipped date checkbox based on whether the shipped date of the order is null or not
-                dtpShippedDate.Checked = order.ShippedDate != null;
-
-                // Set the selected value of the ship via dropdown list to the shipper ID of the order if it's not null, or to 0 if it is null
-                ddlShipVia.SelectedValue = order.ShipVia ?? 0;
-
-                // Set the text of the ship address textbox to the ship address of the order
-                txtAddress.Text = order.ShipAddress;
-
-                // Set the text of the ship city textbox to the ship city of the order
-                txtCity.Text = order.ShipCity;
-
-                // Set the text of the ship country textbox to the ship country of the order
-                txtCountry.Text = order.ShipCountry;
-
-                // Set the text of the freight textbox to the freight of the order if it's not null, or to an empty string if it is null
-                txtFreight.Text = order.Freight?.ToString() ?? "";
+            
             }
-
-             
-
-
-
 
         }
 
